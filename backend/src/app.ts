a5a802830977e683a10f84a57b06b6ded6dc085e
mnayeembasha <mnayeembasha@gmail.com>
2025-08-06 23:35:22 +0530

    init

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,13 +5,21 @@ import orderRoutes from './routes/orderRoutes';
 import { errorHandler } from './middleware/errorMiddleware';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
-
+import fs from "fs";
 // Load environment variables explicitly
-const dotenvResult = dotenv.config();
-if (dotenvResult.error) {
-  console.error('Error loading .env file:', dotenvResult.error);
-  process.exit(1);
+dotenv.config();
+
+
+
+// Only load .env if it exists (e.g., local development)
+if (fs.existsSync(".env")) {
+  dotenv.config();
 }
+// const dotenvResult = dotenv.config();
+// if (dotenvResult.error) {
+//   console.error('Error loading .env file:', dotenvResult.error);
+//   process.exit(1);
+// }
 
 // console.log('Environment variables loaded:', {
 //   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME ? '****' : undefined,
